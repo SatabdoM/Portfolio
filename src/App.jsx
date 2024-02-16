337783656b0e77862247537cb38121d9d60abf1e
SatabdoM <105154633+SatabdoM@users.noreply.github.com>
2024-02-16 14:58:41 +0530

    css

--- src/App.jsx
+++ src/App.jsx
@@ -15,41 +15,39 @@ const App = () => {
   return (
     <div className="bg-gradient w-full">
       <nav
-        className={`fixed  w-full top-0 z-50 flex justify-between px-10  transition-all duration-500 ease-in-out border-b border-0.2 border-richblack-700 bg-richblack-800 bg-opacity-20 backdrop-blur-md `}
+        className={`fixed  w-full top-0 z-50 flex justify-center items-center  md:justify-between px-10  transition-all duration-500 ease-in-out border-b border-0.2 border-richblack-700 bg-richblack-800 bg-opacity-20 backdrop-blur-md `}
       >
         <div>
-          <img src={logo} alt="" className="h-14" />
+          <img src={logo} alt="" className="h-14 " />
         </div>
         <div className="flex gap-10 px-8">
           <ul className="flex gap-8 ">
             <li
               onClick={() => scrollToComponent("about")}
-              className="text-richblack-25 pt-3 text-m font-semibold cursor-pointer transition duration-400 hover:text-tailwindblue-200"
+              className="text-richblack-25  text-m font-semibold cursor-pointer transition duration-400 hover:text-tailwindblue-200"
             >
               About
             </li>
             <li
               onClick={() => scrollToComponent("skills")}
-              className="text-richblack-25 pt-3 text-m font-semibold cursor-pointer transition duration-400 hover:text-tailwindblue-200"
+              className="text-richblack-25  text-m font-semibold cursor-pointer transition duration-400 hover:text-tailwindblue-200"
             >
               Skills
             </li>
             <li
               onClick={() => scrollToComponent("projects")}
-              className="text-richblack-25 pt-3 text-m font-semibold cursor-pointer transition duration-400 hover:text-tailwindblue-200"
+              className="text-richblack-25  text-m font-semibold cursor-pointer transition duration-400 hover:text-tailwindblue-200"
             >
               Projects
             </li>
             <li
               onClick={() => scrollToComponent("education")}
-              className="text-richblack-25 pt-3 text-m font-semibold cursor-pointer transition duration-400 hover:text-tailwindblue-200"
+              className="text-richblack-25  text-m font-semibold cursor-pointer transition duration-400 hover:text-tailwindblue-200"
             >
               Education
             </li>
           </ul>
-          <button className="text-white group transition-all duration-300 hover:text-gray-300 ">
-            Github
-          </button>
+        
         </div>
       </nav>
 
